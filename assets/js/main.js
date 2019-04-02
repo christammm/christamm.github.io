f2eeaa8b7220b9c0ebf4dabf972fa8330b6568f2
Chris <markenyou@aol.com>
2019-04-01 21:35:43 -0400

    more cool changes provided

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $(".side-nav").sticky({topSpacing:10});
+    
+    $(".nav-list-item a").click(function(event) {
+    event.preventDefault();
+    
+    var href=$(this).attr('href');
+        
+    $('html, body').animate({
+        scrollTop: $(""+ href).offset().top
+    }, 500);
+});
   });
