05b4981dba7e73e019de7f2053c3fac7ab3edb9b
Chris <markenyou@aol.com>
2019-04-11 21:11:32 -0400

    responsive changes added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,38 @@
 $(document).ready(function(){
-    $(".side-nav").sticky({topSpacing:10});
+     $(".hamburger-icon").click(function(e){
+        console.log("click!");
+        var belowNav = $(this).parent().find('.below-nav');
+        if(belowNav.hasClass('active')){
+            belowNav.removeClass('active');
+        }else{
+            belowNav.addClass('active');
+        }
+    });
+    
+    $('.nav-list-item a').click(function(event){
+        $('.below-nav').removeClass('active');
+    })
+    
+    $(".side-nav").sticky({topSpacing:0,
+                          responsiveWidth: true,});
+//    $(window).resize(function() {  
+//       var bodyWidth = $(window).width();
+//        if(bodyWidth < 1099){
+//            
+//            
+//        }else{
+//            $(".side-nav").sticky({topSpacing:10});
+//            console.log('sticky started');
+//
+//        }        
+//    });
+    
+//    var initBodyWidth = $(window).width();
+//    if(initBodyWidth > 1099){
+//            $(".side-nav").sticky({topSpacing:10});
+//    }else{
+//        $(".side-nav").sticky({topSpacing:0});
+//    }
     
     $(".nav-list .nav-list-item a").click(function(event) {
     event.preventDefault();
@@ -9,5 +42,7 @@ $(document).ready(function(){
     $('html, body').animate({
         scrollTop: $(""+ href).offset().top
     }, 500);
+        
+   
 });
   });
