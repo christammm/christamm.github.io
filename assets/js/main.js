76ab6bbf929a770819a9951ab77ff114053d1f27
Chris <markenyou@aol.com>
2019-04-11 22:16:01 -0400

    more responsive changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,10 +11,10 @@ $(document).ready(function(){
     
     $('.nav-list-item a').click(function(event){
         $('.below-nav').removeClass('active');
-    })
+    });
     
-    $(".side-nav").sticky({topSpacing:0,
-                          responsiveWidth: true,});
+    
+    $(".side-nav").sticky({topSpacing:0, responsiveWidth: true,});
 //    $(window).resize(function() {  
 //       var bodyWidth = $(window).width();
 //        if(bodyWidth < 1099){
@@ -31,17 +31,24 @@ $(document).ready(function(){
 //    if(initBodyWidth > 1099){
 //            $(".side-nav").sticky({topSpacing:10});
 //    }else{
-//        $(".side-nav").sticky({topSpacing:0});
+//        //$(".side-nav").sticky({topSpacing:0});
 //    }
     
     $(".nav-list .nav-list-item a").click(function(event) {
     event.preventDefault();
     
     var href=$(this).attr('href');
-        
-    $('html, body').animate({
+    
+    if($(window).width() < 1099){
+        $('html, body').animate({
+        scrollTop: $(""+ href).offset().top - 150
+    }, 500);
+    }else{
+        $('html, body').animate({
         scrollTop: $(""+ href).offset().top
     }, 500);
+    }
+    
         
    
 });
